31e354206459340aa30eeeaa687ddb894c2bf936
Ahsan Ali Soomro <149801757+AhsanAli-Soomro@users.noreply.github.com>
2024-10-02 17:08:25 +0500

    update

--- models/Blog.js
+++ models/Blog.js
@@ -4,8 +4,8 @@ const BlogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   image: { type: String },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],  // Make sure this field is an array
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }], 
+  category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }], 
   createdAt: { type: Date, default: Date.now },
 }, { timestamps: true });
 
